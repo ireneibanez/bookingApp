d0351102d2d353efbb5157d442c10452675d7e15
= <=>
2024-06-07 11:46:44 +0200

    feat: Page not found component and routed added

--- bookingApp/src/app/app.routes.ts
+++ bookingApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EventSelectionDetailComponent } from './event-selection-detail/event-selection-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'event', component: EventSelectionDetailComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+];
