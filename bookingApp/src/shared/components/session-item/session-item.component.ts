9eb49800a0d865d8d29474badfdd3f11da5ac001
= <=>
2024-06-07 12:13:28 +0200

    feat: Session item component added

---
+++ bookingApp/src/shared/components/session-item/session-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './session-item.component.html',
+  styleUrl: './session-item.component.scss'
+})
+export class SessionItemComponent {
+
+}
