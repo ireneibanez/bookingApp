70f3f52f9b993d256d5bc7507a3aa06c52483178
= <=>
2024-06-07 11:53:52 +0200

    feat: Routing with router-outlet

--- bookingApp/src/app/app.component.ts
+++ bookingApp/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
